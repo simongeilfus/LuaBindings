cfd0953a1db3fc6f3d59e7a690e8a48a3a223ecf
Simon Geilfus <simongeilfus@MacbookPro.local>
2013-04-17 11:42:10 +0200

    Channel bindings

--- src/BindCinderChannel.cpp
+++ src/BindCinderChannel.cpp
@@ -10,6 +10,16 @@ using namespace luabind;
 
 namespace lua { namespace Bindings {
     
+    void setChannel8uV( Channel8u::Iter& iter, int value ){
+        iter.v() = value;
+    }
+    void setChannel16uV( Channel16u::Iter& iter, int value ){
+        iter.v() = value;
+    }
+    void setChannel32fV( Channel32f::Iter& iter, float value ){
+        iter.v() = value;
+    }
+    
     void bindCinderChannel( lua_State* state ){
         
         module( state ) [
@@ -29,7 +39,28 @@ namespace lua { namespace Bindings {
                          .def( "setPixel", ( void(Channel8u::*)(Vec2i,unsigned char) ) &Channel8u::setValue )
                          
                          .def( "copyFrom", ( void(Channel8u::*)(const Channel8u&,const Area&) ) &Channel8u::copyFrom )
-                         .def( "copyFrom", ( void(Channel8u::*)(const Channel8u&,const Area&, const Vec2i) ) &Channel8u::copyFrom ),
+                         .def( "copyFrom", ( void(Channel8u::*)(const Channel8u&,const Area&, const Vec2i) ) &Channel8u::copyFrom )
+                         
+                         .def( "getIter", ( Channel8u::Iter(Channel8u::*)() ) &Channel8u::getIter )
+                         .def( "getIter", ( Channel8u::Iter(Channel8u::*)(const Area&) ) &Channel8u::getIter ),
+                         
+                         
+                         class_< Channel8u::Iter >("Channel8uIter")
+                         .def(constructor<Channel8u&, const Area &>())
+                         
+                         .def( "v", &setChannel8uV )
+                         //.def( "v", ( uint8_t&(Channel8u::Iter::*)( int32_t, int32_t ) const ) &Channel8u::Iter::v )
+                         //.def( "v", ( uint8_t&(Channel8u::Iter::*)( int32_t, int32_t ) const ) &Channel8u::Iter::vClamped )
+                         
+                         .def( "x", ( int32_t&(Channel8u::Iter::*)() const ) &Channel8u::Iter::x )
+                         .def( "y", ( int32_t&(Channel8u::Iter::*)() const ) &Channel8u::Iter::y )
+                         .def( "getPos", ( Vec2i&(Channel8u::Iter::*)() const ) &Channel8u::Iter::getPos )
+                         
+                         .def( "pixel", ( bool(Channel8u::Iter::*)() ) &Channel8u::Iter::pixel )
+                         .def( "line", ( bool(Channel8u::Iter::*)() ) &Channel8u::Iter::line )
+                         
+                         .def( "getWidth", ( int32_t(Channel8u::Iter::*)() ) &Channel8u::Iter::getWidth )
+                         .def( "getHeight", ( int32_t(Channel8u::Iter::*)() ) &Channel8u::Iter::getHeight ),
                          
                          
                          class_< Channel16u >("Channel16u")
@@ -45,10 +76,31 @@ namespace lua { namespace Bindings {
                          .def( "clone", ( Channel16u(Channel16u::*)( bool ) const ) &Channel16u::clone )
                          .def( "clone", ( Channel16u(Channel16u::*)( const Area&, bool )const ) &Channel16u::clone )
                          
-                         .def( "setPixel", ( void(Channel8u::*)(Vec2i,unsigned char) ) &Channel8u::setValue )
+                         .def( "setPixel", ( void(Channel16u::*)(Vec2i,unsigned char) ) &Channel16u::setValue )
                          
                          .def( "copyFrom", ( void(Channel16u::*)(const Channel16u&,const Area&) ) &Channel16u::copyFrom )
-                         .def( "copyFrom", ( void(Channel16u::*)(const Channel16u&,const Area&, const Vec2i) ) &Channel16u::copyFrom ),
+                         .def( "copyFrom", ( void(Channel16u::*)(const Channel16u&,const Area&, const Vec2i) ) &Channel16u::copyFrom )
+                         
+                         .def( "getIter", ( Channel16u::Iter(Channel16u::*)() ) &Channel16u::getIter )
+                         .def( "getIter", ( Channel16u::Iter(Channel16u::*)(const Area&) ) &Channel16u::getIter ),
+                         
+                         
+                         class_< Channel16u::Iter >("Channel16uIter")
+                         .def(constructor<Channel16u&, const Area &>())
+                         
+                         .def( "v", &setChannel16uV )
+                         //.def( "v", ( uint16_t&(Channel16u::Iter::*)( int32_t, int32_t ) const ) &Channel16u::Iter::v )
+                         //.def( "v", ( uint16_t&(Channel16u::Iter::*)( int32_t, int32_t ) const ) &Channel16u::Iter::vClamped )
+                         
+                         .def( "x", ( int32_t&(Channel16u::Iter::*)() const ) &Channel16u::Iter::x )
+                         .def( "y", ( int32_t&(Channel16u::Iter::*)() const ) &Channel16u::Iter::y )
+                         .def( "getPos", ( Vec2i&(Channel16u::Iter::*)() const ) &Channel16u::Iter::getPos )
+                         
+                         .def( "pixel", ( bool(Channel16u::Iter::*)() ) &Channel16u::Iter::pixel )
+                         .def( "line", ( bool(Channel16u::Iter::*)() ) &Channel16u::Iter::line )
+                         
+                         .def( "getWidth", ( int32_t(Channel16u::Iter::*)() ) &Channel16u::Iter::getWidth )
+                         .def( "getHeight", ( int32_t(Channel16u::Iter::*)() ) &Channel16u::Iter::getHeight ),
                          
                          
                          class_< Channel32f >("Channel32f")
@@ -64,10 +116,32 @@ namespace lua { namespace Bindings {
                          .def( "clone", ( Channel32f(Channel32f::*)( bool ) const ) &Channel32f::clone )
                          .def( "clone", ( Channel32f(Channel32f::*)( const Area&, bool )const ) &Channel32f::clone )
                          
-                         .def( "setPixel", ( void(Channel8u::*)(Vec2i,unsigned char) ) &Channel8u::setValue )
+                         .def( "setPixel", ( void(Channel32f::*)(Vec2i,unsigned char) ) &Channel32f::setValue )
                          
                          .def( "copyFrom", ( void(Channel32f::*)(const Channel32f&,const Area&) ) &Channel32f::copyFrom )
                          .def( "copyFrom", ( void(Channel32f::*)(const Channel32f&,const Area&, const Vec2i) ) &Channel32f::copyFrom )
+                         
+                         .def( "getIter", ( Channel32f::Iter(Channel32f::*)() ) &Channel32f::getIter )
+                         .def( "getIter", ( Channel32f::Iter(Channel32f::*)(const Area&) ) &Channel32f::getIter ),
+                         
+                         
+                         class_< Channel32f::Iter >("Channel32fIter")
+                         .def(constructor<Channel32f&, const Area &>())
+                         
+                         .def( "v", &setChannel32fV )
+                         //.def( "v", ( float&(Channel32f::Iter::*)( int32_t, int32_t ) const ) &Channel32f::Iter::v )
+                         //.def( "v", ( float&(Channel32f::Iter::*)( int32_t, int32_t ) const ) &Channel32f::Iter::vClamped )
+                         
+                         .def( "x", &Channel32f::Iter::x )
+                         .def( "y", &Channel32f::Iter::y )
+                         .def( "getPos", &Channel32f::Iter::getPos )
+                         
+                         .def( "pixel", ( bool(Channel32f::Iter::*)() ) &Channel32f::Iter::pixel )
+                         .def( "line", ( bool(Channel32f::Iter::*)() ) &Channel32f::Iter::line )
+                         
+                         .def( "getWidth", &Channel32f::Iter::getWidth )
+                         .def( "getHeight", &Channel32f::Iter::getHeight )
+                         
                          
                          ];
     }
